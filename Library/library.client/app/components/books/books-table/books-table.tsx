4c59f43a27ceabb00e12546d35fb252130f61eeb
Brzeska <Antonina.Brzeska@dnv.com>
2024-12-22 11:59:55 +0100

    axios loading books

--- Library/library.client/app/components/books/books-table/books-table.tsx
+++ Library/library.client/app/components/books/books-table/books-table.tsx
@@ -1,5 +1,28 @@
+"use client";
+import { Book } from "@/app/models/books-model";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const BooksTable = () => {
-  return <>Books table</>;
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Book[]>("api/books")
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      Books table
+      {books && <pre>{JSON.stringify(books, null, 2)}</pre>}
+    </>
+  );
 };
 
 export default BooksTable;
